d0384ed8e01c987071cf476996eeb865c1eeb2aa
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-06 18:07:13 +0100

    Task 5

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_Google.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T2_Google.java
@@ -1,5 +1,40 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
 public class T2_Google {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://yahoo.com");
+        WebElement agree = driver.findElement(By.name("agree"));
+        agree.click();
+
+        WebElement googleSeachBox = driver.findElement(By.name("p"));
+        googleSeachBox.sendKeys("The best company ever as a tester" + Keys.ENTER);
+        String expectedInTitle = "The";
+        String actualTitle = driver.getTitle();
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        if (actualTitle.startsWith(expectedInTitle)) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Wrong");
+        }
+
+
+    }
 
 }
---
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T4_LibraryLoginPage.java
@@ -0,0 +1,34 @@
+package com.cydeo.tests.day2_locators_getText_getAttribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import javax.swing.*;
+import java.awt.*;
+import java.sql.DataTruncation;
+import java.sql.Driver;
+import java.sql.DriverManager;
+
+public class T4_LibraryLoginPage {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://library2.cybertekschool.com/login.html");
+        WebElement inputEmail = driver.findElement(By.className("form-control"));
+        inputEmail.sendKeys("wrong@gmail.com");
+        WebElement password = driver.findElement(By.id("inputPassword"));
+        password.sendKeys("123kjasdn");
+
+        WebElement clickButton = driver.findElement(By.className("btn"));
+        clickButton.click();
+
+    }
+}
