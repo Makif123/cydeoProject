6a2d0561ffc2d23e39110752293c554adb2aeda9
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-06 08:53:36 +0100

    Task 1

--- src/test/java/com/cydeo/tests/day1_selenium_intro/YahooTitleVerification.java
+++ src/test/java/com/cydeo/tests/day1_selenium_intro/YahooTitleVerification.java
@@ -24,25 +24,6 @@ public class YahooTitleVerification {
             System.out.println("Titsle is not accepted Verification not PASS");
         }
     }
-    class A {
 
-        private A() { }
-
-        void invokeB() {
-            B objB = new B(); // (1)
-            int b = objB.b;   // (2)
-            int c = objB.c;   // (3)
-        }
-    }
-
-    class B {
-
-        protected int b;
-        private int c;
-
-        public void invokeA() {
-            A objA = new A(); // (4)
-        }
-    }
 
 }
--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T1_CydeoVerifications.java
@@ -1,8 +1,33 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.BrowserType;
+
 public class T1_CydeoVerifications {
 
     public static void main(String[] args) {
+//        TC #1: Cydeo practice tool verifications
+//                Open Chrome Browser
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.navigate().to("https://practice.cydeo.com");
 
+        String expectedURL = "cydeo";
+
+        String actualURL = driver.getCurrentUrl();
+        String actualTitle = driver.getTitle();
+        System.out.println(actualURL);
+        System.out.println(expectedURL);
+        if(actualURL.equals(expectedURL)){
+        System.out.println("URL verificatiob PASSED!");
+        } else {
+            System.out.println("URL verification not Passed!");
+        }
     }
 }
