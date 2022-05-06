1601320b64bbd4d6c37bd88e7bf13f31b0363149
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-06 14:03:50 +0100

    Task 3

--- src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_BackAndForthNavigation.java
+++ src/test/java/com/cydeo/tests/day2_locators_getText_getAttribute/T3_BackAndForthNavigation.java
@@ -1,20 +1,43 @@
 package com.cydeo.tests.day2_locators_getText_getAttribute;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.xml.sax.Locator;
 
 public class T3_BackAndForthNavigation {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
+
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-
         driver.navigate().to("https://practice.cydeo.com");
+        WebElement abTestLink = driver.findElement(By.linkText("A/B Testing"));
+
+        abTestLink.click();
+        String expectedTitle = "No A/B Test";
+        String actualTitle = driver.getTitle();
+        System.out.println(driver.getCurrentUrl());
+
+        if(actualTitle.equals(expectedTitle)) {
+            System.out.println("Pass");
+        }else {
+            System.out.println("UNVALID");
+        }
+
+        driver.navigate().back();
+
+        expectedTitle = "Practice";
+
+        if (actualTitle.equals(expectedTitle)) {
+            System.out.println("Pass");
+    }else {
+        System.out.println("UNVALID");
+    }
 
 
 
