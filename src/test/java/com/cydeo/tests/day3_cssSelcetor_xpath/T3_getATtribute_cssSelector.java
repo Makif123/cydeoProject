c10c48db53bff598358bd9069285a608071b5032
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-12 23:23:05 +0100

    day3 task3 getAttribute and cssSelector

--- src/test/java/com/cydeo/tests/day3_cssSelcetor_xpath/T3_getATtribute_cssSelector.java
+++ src/test/java/com/cydeo/tests/day3_cssSelcetor_xpath/T3_getATtribute_cssSelector.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day3_cssSelcetor_xpath;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class T3_getATtribute_cssSelector {
     public static void main(String[] args) {
@@ -9,6 +11,17 @@ public class T3_getATtribute_cssSelector {
         driver.manage().window().maximize();
 
         driver.navigate().to("https://login1.nextbasecrm.com/");
+
+        // tagname[attribute='value']
+
+        WebElement signInButton = driver.findElement(By.cssSelector("input[value='Log In']"));
+
+        String expectedButtonText = "Log In";
+        //Getting the value of the attribute "value"
+        String actualButtonText = signInButton.getAttribute("value");
+
+        System.out.println(actualButtonText);
+
     }
 
 
