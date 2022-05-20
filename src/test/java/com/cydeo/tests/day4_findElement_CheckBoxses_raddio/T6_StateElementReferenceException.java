865095684551a8a18ab5020250039afe4d83839d
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-20 18:43:18 +0100

    day4 task 6 checkboxes with use StateElementReferenceException that we relocate again which is lost WebElement

--- src/test/java/com/cydeo/tests/day4_findElement_CheckBoxses_raddio/T6_StateElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElement_CheckBoxses_raddio/T6_StateElementReferenceException.java
@@ -10,14 +10,20 @@ import java.util.concurrent.TimeUnit;
 public class T6_StateElementReferenceException {
     public static void main(String[] args) {
         WebDriver driver = WebDriverFactory.getDriver("chrome");
-        driver.manage().window().maximize();
+
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to("https://practice.cydeo.com/abtest");
-
+        driver.manage().window().maximize();
 
         WebElement cydeoLink = driver.findElement(By.xpath("//a[text() = 'CYDEO']"));
+
+
         System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
-//        driver.navigate().refresh();
-//        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+        driver.navigate().refresh();
+        cydeoLink = driver.findElement(By.xpath("//a[text() = 'CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+
+
+
     }
 }
