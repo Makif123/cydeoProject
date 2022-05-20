0870ada7a9499b2c7002fe3f541361e8a1fd50b8
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-20 18:37:29 +0100

    day4 task 6 checkboxes without use StateElementReferenceException

--- src/test/java/com/cydeo/tests/day4_findElement_CheckBoxses_raddio/T6_StateElementReferenceException.java
+++ src/test/java/com/cydeo/tests/day4_findElement_CheckBoxses_raddio/T6_StateElementReferenceException.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day4_findElement_CheckBoxses_raddio;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.TimeUnit;
 
@@ -11,5 +13,11 @@ public class T6_StateElementReferenceException {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to("https://practice.cydeo.com/abtest");
+
+
+        WebElement cydeoLink = driver.findElement(By.xpath("//a[text() = 'CYDEO']"));
+        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
+//        driver.navigate().refresh();
+//        System.out.println("cydeoLink.isDisplayed() = " + cydeoLink.isDisplayed());
     }
 }
