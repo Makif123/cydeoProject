6782e216eed428859ac14b0a64dc43903d44eb19
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-21 22:36:03 +0100

    day5 task 3  I use the  List to add all the WebElements

---
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T3_RadioButton_cont.java
@@ -0,0 +1,39 @@
+package com.cydeo.tests.day5_testNG_intro_dropdowns;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class T3_RadioButton_cont {
+    public static void main(String[] args) {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/radio_buttons");
+
+        List<WebElement> supportRadioButtons = driver.findElements(By.name("sports"));
+
+        for (WebElement each : supportRadioButtons) {
+
+           String eachId =  each.getAttribute("id");
+
+            System.out.println("eachId = " + eachId);
+
+            if (eachId.equals("hockey")) {
+                each.click();
+                System.out.println("Hockey is selected : " + each.isSelected());
+                break;
+            }
+        }
+
+    }
+}
