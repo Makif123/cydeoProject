ae5237d45d6bc98ac57759366117b791f49b10dd
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-22 20:13:10 +0100

    T4 Simple dropdown test can use the Select object

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/T4_SimpleDropdowns.java
@@ -1,16 +1,22 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class T4_SimpleDropdowns {
+    WebDriver driver;
+
     @BeforeMethod
-    public void setupMethod(){
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
@@ -19,7 +25,15 @@ public class T4_SimpleDropdowns {
 
 
     @Test
-    public void simpleDropdownTest(){
+    public void simpleDropdownTest() {
 
+        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id = 'dropdown']"))); // Basically it means that we use Select object to take dropdown and then use the method to change webelement
+
+        WebElement currentlySelectedOption = simpleDropdown.getFirstSelectedOption();
+
+        String actualSimpleDropDownText = currentlySelectedOption.getText();
+        String expextedSimpleDropDownText = "Please select an option";
+
+        Assert.assertEquals(actualSimpleDropDownText, expextedSimpleDropDownText, "it's not matching");
     }
 }
