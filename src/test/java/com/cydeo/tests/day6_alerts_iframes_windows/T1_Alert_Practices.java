1cbee231966829920fb18f26809f76877a973807
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-30 18:36:02 +0100

    day6 T1

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T1_Alert_Practices.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T1_Alert_Practices {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @Test
+    public void alert_test1() {
+
+
+
+    }
+
+
+    @AfterMethod
+    public void closeMethod() {
+        driver.close();
+    }
+}
