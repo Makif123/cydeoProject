470fcd56ee05d8040050d137c0a1cc439a0de3d7
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-30 17:21:08 +0100

    day6 Task 6 date dropdowns

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropDownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropDownPractices.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -14,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 public class DropDownPractices {
     WebDriver driver;
-    Select selectDropdown = new Select(driver.findElement(By.xpath("//select[@id ='state']")));
+//    Select selectDropdown = new Select(driver.findElement(By.xpath("//select[@id ='state']")));
     String expectedDropDown = "California";
 
     @BeforeMethod
@@ -29,34 +30,39 @@ public class DropDownPractices {
     }
 
     @Test
-    public void Test1() {
+    public void Test1()   {
+        Select selectDropdown = new Select(driver.findElement(By.xpath("//select[@id ='state']")));
+
 //        Select simpleDropdown = new Select(driver.findElement(By.xpath("//select[@id = 'dropdown']")));
         selectDropdown.selectByVisibleText("Illinois");
     }
 
     @Test
-    public void Test2() {
-
-        selectDropdown.selectByVisibleText("Virginia");
+    public void Test2() throws InterruptedException {
+        Thread.sleep(1000);
+        Select selectDropdown = new Select(driver.findElement(By.xpath("//select[@id ='state']")));
+        selectDropdown.selectByValue("VA");
     }
 
     // How do you get default selected value?
 
 
     @Test
-    public void Test3() {
-
-        selectDropdown.selectByVisibleText("California");
-
-
+    public void Test3() throws InterruptedException {
+        Thread.sleep(1000);
+        Select selectDropdown = new Select(driver.findElement(By.xpath("//select[@id ='state']")));
+       selectDropdown.selectByIndex(5);
+       String expectedOptionText = "California";
+       String actualOptionText = selectDropdown.getFirstSelectedOption().getText();
+        Assert.assertEquals(expectedOptionText,actualOptionText);
 
 
     }
 
     @Test
-    public void Test4(){
+    public void Test6() {
 
-        WebElement wrappedElement = selectDropdown.getWrappedElement();
+//        WebElement wrappedElement = selectDropdown.getWrappedElement();
 
 
     }
