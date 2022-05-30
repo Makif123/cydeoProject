0e370cd66ccdaeeb0083e50c563089401bbc2d10
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-30 19:43:08 +0100

    day6 T2 iframe

---
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_Iframes.java
@@ -0,0 +1,32 @@
+package com.cydeo.tests.day6_alerts_iframes_windows;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class T4_Iframes {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/iframe");
+
+    }
+
+
+    @Test
+    public void test_iframe() {
+
+
+
+
+    }
+
+}
