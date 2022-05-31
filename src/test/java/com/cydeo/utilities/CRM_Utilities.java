c8663f5c9d3629ec989de180f640a88ff59215b9
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-31 19:26:27 +0100

    day7 T3_CRM_LOGIN and use login method

--- src/test/java/com/cydeo/utilities/CRM_Utilities.java
+++ src/test/java/com/cydeo/utilities/CRM_Utilities.java
@@ -15,10 +15,19 @@ public class CRM_Utilities {
         userName.sendKeys("helpdesk1@cybertekschool.com");
         userPassword.sendKeys("UserUser");
         loginButton.click();
-
-
-
-
-
     }
+    public static void crm_login(WebDriver driver, String userName , String userPassword){
+
+        driver.get("https://login1.nextbasecrm.com/");
+        WebElement inputUserName = driver.findElement(By.xpath("//input[@name = 'USER_LOGIN']"));
+        WebElement inputPassword = driver.findElement(By.xpath("//input[@name = 'USER_PASSWORD']"));
+        WebElement loginButton = driver.findElement(By.xpath("//input[@class='login-btn']"));
+
+        inputUserName.sendKeys(userName);
+        inputPassword.sendKeys(userPassword);
+        loginButton.click();
+    }
+
+
+
 }
