596084e27d8f7f751561ddbe2691b12cacddfbf9
Junior <94207328+Makif123@users.noreply.github.com>
2022-05-31 18:43:59 +0100

    day7 BrowserUtilities sleep method and switch method

--- src/test/java/com/cydeo/utilities/BrowserUtilities.java
+++ src/test/java/com/cydeo/utilities/BrowserUtilities.java
@@ -1,5 +1,10 @@
 package com.cydeo.utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+import java.util.Set;
+
 public class BrowserUtilities {
     public static void sleep(int time) {
         time *= 1000;
@@ -11,4 +16,22 @@ public class BrowserUtilities {
         }
 
     }
+
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedInTitle) {
+
+        Set<String> allWindowsHandle = driver.getWindowHandles();
+        for (String each : allWindowsHandle) {
+            driver.switchTo().window(each);
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+        }
+        String actualTitle = driver.getTitle();
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
+
+    }
+
 }
