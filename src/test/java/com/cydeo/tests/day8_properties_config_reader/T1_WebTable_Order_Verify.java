1d840b503e6c8378e7595d8cded58e704ba8f1c1
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-01 14:49:29 +0100

    day7 T1_locate name and tables inside the webtables

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -21,6 +24,17 @@ public class T1_WebTable_Order_Verify {
     @Test
     public void order_name_verify_test() {
         //2. Verify Bob's name is listed as expected.
+
+        WebElement Bobs = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']"));
+        WebElement BobsDate = driver.findElement(By.xpath("//table[@class='SampleTable']//td[.='Bob Martin']/..//td[5]"));
+
+        String actualText = Bobs.getText();
+        String expectedText = "Bob Martin";
+        String actualDate = BobsDate.getText();
+
+        String expectedDate = "12/31/2021";
+        Assert.assertEquals(actualText, expectedText);
+        Assert.assertEquals(actualDate, expectedDate);
 
     }
 }
