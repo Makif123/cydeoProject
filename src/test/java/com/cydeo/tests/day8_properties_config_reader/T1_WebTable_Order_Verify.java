1325cff347858274db1699b3d9e4177996f7a098
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-01 14:19:47 +0100

    day7 T1

--- src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
+++ src/test/java/com/cydeo/tests/day8_properties_config_reader/T1_WebTable_Order_Verify.java
@@ -1,9 +1,26 @@
 package com.cydeo.tests.day8_properties_config_reader;
 
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class T1_WebTable_Order_Verify {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod() {
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://practice.cydeo.com/web-tables");
+    }
 
     @Test
-    public void order_name_verify_test(){}
+    public void order_name_verify_test() {
+        //2. Verify Bob's name is listed as expected.
+
+    }
 }
