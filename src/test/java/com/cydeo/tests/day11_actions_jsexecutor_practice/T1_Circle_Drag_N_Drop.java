7d2f22a5b2651a74247e0d4d968c823ac74766fe
Junior <94207328+Makif123@users.noreply.github.com>
2022-06-08 19:32:28 +0100

    I added actions to hold some locator and move to another

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -1,2 +1,33 @@
-package com.cydeo.tests.day11_actions_jsexecutor_practice;public class T1_Circle_Drag_N_Drop {
+package com.cydeo.tests.day11_actions_jsexecutor_practice;
+
+import com.cydeo.utilities.BrowserUtilities;
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class T1_Circle_Drag_N_Drop {
+    @Test
+    public void test1() {
+        Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
+        BrowserUtilities.sleep(4);
+        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.xpath("//*[@id=\"onetrust-accept-btn-handler\"]"));
+
+
+        acceptCookiesButton.click();
+        WebElement draggable = Driver.getDriver().findElement(By.id("draggable"));
+        WebElement dropTarget = Driver.getDriver().findElement(By.id("droptarget"));
+        Actions actions = new Actions(Driver.getDriver());
+
+
+//        actions.dragAndDrop(draggable,dropTarget).perform();
+
+        actions.clickAndHold(draggable).
+                pause(2000).moveToElement(dropTarget).
+                pause(2000).release().perform();
+        BrowserUtilities.sleep(3);
+        Driver.closeDriver();
+    }
 }
